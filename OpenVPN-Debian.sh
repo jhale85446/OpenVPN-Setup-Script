b80d3e0567a2b35bd784e43c2cd93562cf862c5f
Josh Hale <jhale85446@gmail.com>
2016-06-17 22:39:21 -0500

    Add Line Breaks and Unzip

--- OpenVPN-Debian.sh
+++ OpenVPN-Debian.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "Installing OpenVPN and Easy-RSA"
+printf "Installing OpenVPN and Easy-RSA\n"
+printf "-----------------------------------------------------------------\n"
 apt-get update
 apt-get install -y openvpn easy-rsa
+printf "-----------------------------------------------------------------\n"
 
+printf "\nUnzipping the sample server config file to use as a base.\n"
+printf "-----------------------------------------------------------------\n"
+gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz > /etc/openvpn/server.conf
+printf "-----------------------------------------------------------------\n"
