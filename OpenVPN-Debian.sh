bb6b9270b57cb23974ef5ede300bf313ca3caaac
Josh Hale <jhale85446@gmail.com>
2016-06-17 22:48:43 -0500

    Add unzip and check

--- OpenVPN-Debian.sh
+++ OpenVPN-Debian.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 printf "Installing OpenVPN and Easy-RSA\n"
-printf "-----------------------------------------------------------------\n"
+printf "_________________________________________________________________\n"
 apt-get update
 apt-get install -y openvpn easy-rsa
-printf "-----------------------------------------------------------------\n"
+printf "_________________________________________________________________\n"
 
 printf "\nUnzipping the sample server config file to use as a base.\n"
-printf "-----------------------------------------------------------------\n"
+printf "_________________________________________________________________\n"
 gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz > /etc/openvpn/server.conf
-printf "-----------------------------------------------------------------\n"
+[ -f /etc/openvpn/server.conf ] && echo "Done - Moving on" || echo "Something messed up."
+printf "_________________________________________________________________\n"
