d0b093635eaa6c5f6d355001b56401cbf6a22bd3
Josh Hale <jhale85446@gmail.com>
2016-06-20 15:04:23 -0500

    Add Protocol and Port Change to Server.conf

--- OpenVPN-Debian.sh
+++ OpenVPN-Debian.sh
@@ -140,35 +140,53 @@ for (( i=0;i<$count;i++)); do
     printf "\t${subnets[${i}]}\n"
 done
 
-#printf "\nInstalling OpenVPN and Easy-RSA\n\n"
+printf "\nInstalling OpenVPN and Easy-RSA\n\n"
 #apt-get update
 #apt-get install -y openvpn easy-rsa
-#printf "_________________________________________________________________\n"
+printf "_________________________________________________________________\n"
 
-#printf "\nUnzipping the sample server config file to use as a base.\n\n"
-#gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz > /etc/openvpn/server.conf
+printf "\nUnzipping the sample server config file to use as a base.\n\n"
+gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz > /etc/openvpn/server.conf
 
-#if [ -f /etc/openvpn/server.conf ]; then
-#  printf "Done - Moving on\n"
-#else
-#  printf "Something messed up. Exiting.\n"
-#  exit 1
-#fi
+if [ -f /etc/openvpn/server.conf ]; then
+  printf "Done - Moving on\n"
+else
+  printf "Something messed up. Exiting.\n"
+  exit 1
+fi
 
-#printf "_________________________________________________________________\n"
+printf "_________________________________________________________________\n"
 
-#printf "\nChanging Initial Server Settings.\n\n"
-#printf "Changing Encryption Level: 'dh dh1024.pem' -> 'dh dh2048.pem'\n"
-#sed -i 's/dh dh1024.pem/dh dh2048.pem/g' /etc/openvpn/server.conf
-#printf "Allowing Web Traffic Forwarding\n"
-#sed -i 's/;push "redirect-gateway def1 bypass-dhcp"/push "redirect-gateway def1 bypass-dhcp"/g' /etc/openvpn/server.conf
-#printf "Setting Clients to Use OpenDNS when possible\n"
-#sed -i 's/;push "dhcp-option DNS 208.67.222.222"/push "dhcp-option DNS 208.67.222.222"/g' /etc/openvpn/server.conf
-#sed -i 's/;push "dhcp-option DNS 208.67.220.220"/push "dhcp-option DNS 208.67.220.220"/g' /etc/openvpn/server.conf
-#printf "Setting Server Permissions\n"
-#sed -i 's/;user nobody/user nobody/g' /etc/openvpn/server.conf
-#sed -i 's/;group nogroup/group nogroup/g' /etc/openvpn/server.conf
-#printf "_________________________________________________________________\n"
+printf "\nChanging Initial Server Settings.\n\n"
+printf "Changing Encryption Level: 'dh dh1024.pem' -> 'dh dh2048.pem'\n"
+sed -i 's/dh dh1024.pem/dh dh2048.pem/g' /etc/openvpn/server.conf
+
+printf "Allowing Web Traffic Forwarding\n"
+sed -i 's/;push "redirect-gateway def1 bypass-dhcp"/push "redirect-gateway def1 bypass-dhcp"/g' /etc/openvpn/server.conf
+
+printf "Setting Clients to Use OpenDNS when possible\n"
+sed -i 's/;push "dhcp-option DNS 208.67.222.222"/push "dhcp-option DNS 208.67.222.222"/g' /etc/openvpn/server.conf
+sed -i 's/;push "dhcp-option DNS 208.67.220.220"/push "dhcp-option DNS 208.67.220.220"/g' /etc/openvpn/server.conf
+
+printf "Setting Server Permissions\n"
+sed -i 's/;user nobody/user nobody/g' /etc/openvpn/server.conf
+sed -i 's/;group nogroup/group nogroup/g' /etc/openvpn/server.conf
+
+printf "Setting VPN Traffic Type to $traffic\n"
+if [ "$traffic" == "UDP" ]; then
+  sed -i 's/;proto udp/proto udp/g' /etc/openvpn/server.conf
+  sed -i 's/proto tcp/;proto tcp/g' /etc/openvpn/server.conf
+else
+  sed -i 's/proto udp/;proto udp/g' /etc/openvpn/server.conf
+  sed -i 's/;proto tcp/proto tcp/g' /etc/openvpn/server.conf
+fi
+
+printf "Setting VPN Port to $port\n"
+sed -i 's/^port.*/port '$port'/g' /etc/openvpn/server.conf
+
+printf "_________________________________________________________________\n"
+
+exit 0
 
 
 
